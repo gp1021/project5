8b988ee021e7a58b0dbe6fb5b24b00f6aaf5ec0c
gp1021 <60611487+gp1021@users.noreply.github.com>
2020-05-23 21:09:05 -0400

    Update RandomNumbers.java

--- Arithmetic Problem Generator Lab/src/lab2/RandomNumbers.java
+++ Arithmetic Problem Generator Lab/src/lab2/RandomNumbers.java
@@ -1,10 +1,12 @@
 package lab2;
 
 public class RandomNumbers {
+	//instance variables
 	int x = (int)(Math.random() * 9);
 	int y = (int)(Math.random() * 9);
 	int sign = (int)(Math.random() * 2); //1 = addition, 2 = subtraction
 	
+	//getter methods
 	public int getX() {
 		return x;
 	}
@@ -17,10 +19,12 @@ public class RandomNumbers {
 		return sign;
 	}
 	
+	//constructor
 	public RandomNumbers() {
 		
 	}
 	
+	//prints out a random arithmetic problem (either addition or subtraction), based on numbers 0 through 9, with only positive solutions
 	public String problemGenerator() {
 		String result;
 		if (sign == 1) {
